5fa1357ae76556ec228afb85a3a0486bc327bcd9
balaji <balajis@gmail.com>
2023-05-11 23:56:54 +0530

    manual cart

--- consultancy_project/src/App.js
+++ consultancy_project/src/App.js
@@ -14,6 +14,7 @@ import ContactForm from './components/Contact';
 import Admin from './components/Admin';
 import Products from './components/Products';
 import Manual from './components/Manuals';
+import CartPopup from './components/Cart';
 //import Footer from './components/Footer'
 
 const App = () => {
@@ -30,6 +31,7 @@ const App = () => {
       <Route path ='/adminadd' element={<Admin/>}></Route>
       <Route path='/contact' element={<ContactForm/>}></Route>
       <Route path='/register' element={<Register/>}></Route>
+      <Route path='/cart' element={<CartPopup/>}></Route>
     <Route path='/Products' element={<Product/>}></Route>
     <Route path='/manual/:id' element={<Manual/>}></Route>
     </Routes> 
--- consultancy_project/src/components/Products.js
+++ consultancy_project/src/components/Products.js
@@ -1,121 +1,112 @@
- //import React from 'react';
+import React, { useEffect, useState } from "react";
+import { ImPriceTag } from 'react-icons/im';
+import { FcAbout } from 'react-icons/fc';
+import { Link } from "react-router-dom";
+import { MDBBtn } from 'mdb-react-ui-kit';
+import CartPopup from "./Cart";
 
-// import {BiBed,BiBath,BiArea} from 'react-icons/bi'
-// import {ImPriceTag} from 'react-icons/im'
-// import {FcAbout} from 'react-icons/fc'
-// const Products = ({product}) => {
-//   const {name,cost,about,img,}=product;
-
-//   return (
-//   <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition ' >
-//     <img className='mb-8'src={img} alt=''></img>
-//     <div className='mb-4 flex-gap-x-2 text-sm'>
-//       <div>{name}</div>
-      
-
-//     </div>
-   
-//     <div className='flex gap-x-4 my-4'>
-      
-//     </div>
-//     <div className='flex items-center gap-1'>
-//       <div>
-//       <ImPriceTag/>
-//       </div>
-//       <div>{cost}</div>
-//     </div>
-//     <div className='flex items-center gap-1'>
-//       <div>
-//       <FcAbout/>
-//       </div>
-//       <div>{about}</div>
-//     </div>
-   
-//   </div>);
-// };
-import React, { useEffect, useState } from "react"
-import axios from "axios"
-import {ImPriceTag} from 'react-icons/im'
-import {FcAbout} from 'react-icons/fc'
-import { Link } from "react-router-dom"
 const Products = () => {
-  
+
   const [product, setproduct] = useState([]);
-  const [loading,setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
-  const fetchUserData = async() => {
+  const fetchUserData = async () => {
     setLoading(true);
-    let response = await axios.get("http://localhost:7000/api/v1/products")
-          .then(res => { console.log(res); setproduct(res.data.data["Products"])}).catch((err)=>{
-            console.log(err)
-          })
-
-          setLoading(false)
+    try {
+      const response = await fetch("http://localhost:7000/api/v1/products");
+      const data = await response.json();
+      setproduct(data.data["Products"]);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   }
-  
 
   useEffect(() => {
     fetchUserData()
   }, [])
-// console.log(product.data['Products'])
 
-if(loading)
-{
-  return <div>Loading...</div>
-}
+  useEffect(() => {
+    const items = localStorage.getItem('cartItems');
 
-// if(product)
-// {
-//   console.log(product.data["Products"])
-// }
+    if (items) {
+      
+      const cartItems = JSON.parse(items);
+      setCartItems(cartItems);
+      // continue processing cartItems data
+    } else {
+      // handle empty or null data
+    }
+    
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  function handleAddToCart(product) {
+    const existingItem = cartItems.find(item => item._id === product._id);
+
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  }
+
+  function handleRemoveFromCart(item) {
+    const updatedCartItems = cartItems.filter(cartItem => cartItem._id !== item._id);
+    setCartItems(updatedCartItems);
+  }
+
+  function handleClearCart() {
+    setCartItems([]);
+  }
 
   return (
-    
     <div>
-      <></>
       <h2 className="text-3xl font-bold mb-4 text-center">Products</h2>
-      
       {product.length > 0 && (
-        
-              <div style={{display:"grid",gridTemplateColumns:"repeat(3, 1fr)" }} >
-              {product.map(product => (
-                <>
-                {/* <img src={product.image} />
-                <li key={product.name}>{product.name + product.cost}</li> */}
-                <Link to={`/manual/${product._id}`} >
-                <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition ' >
-      <img className='mb-8'src={product.image} alt='' style={{width:"fit-content"}}></img>
-        <div className='mb-4 flex-gap-x-2 text-sm'>
-        <div>{product.name}</div>
-          
-
-      </div>
-      
-      <div className='flex gap-x-4 my-4'>
+        <div style={{display:"grid",gridTemplateColumns:"repeat(3, 1fr)"}}>
+          {product.map(product => (
+            <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition' key={product._id}>
+              <img className='mb-8' src={product.image} alt='' style={{width:"fit-content"}}></img>
+              <div className='mb-4 flex-gap-x-2 text-sm'>
+                <div>{product.name}</div>
+              </div>
+              <div className='flex gap-x-4 my-4'></div>
+              <div className='flex items-center gap-1'>
+                <div>
+                  <ImPriceTag/>
+                </div>
+                <div>{product.cost}</div>
+              </div>
+              <div className='flex items-center gap-1'>
+                <div>
+                  <FcAbout/>
+                </div>
+                <div>{product.shortDescription}</div>
+              </div>
+              <br></br>
+              <div style={{"textAlign":"center","color":"red"}}>
+                <Link to={`/manual/${product._id}`}>Manual (Full details)</Link>
+                <div>
+                  <MDBBtn className='me-1' onClick={() => handleAddToCart(product)}>Add to cart</MDBBtn>
+                </div>
+              </div>
             </div>
-        <div className='flex items-center gap-1'>
-          <div>
-          <ImPriceTag/>
-          </div>
-          <div>{product.cost}</div>
+          ))}
         </div>
-        <div className='flex items-center gap-1'>
-          <div>
-          <FcAbout/>
-          </div>
-          <div>{product.shortDescription}</div>
-        </div>
-      
-      </div>
-      </Link>
-                </>
-              ))}
-            </div>
       )}
-    </div>
-  );
-          }
-
-
+      <div style={{ textAlign: "center" }}>
+       <Link to="/cart">
+<MDBBtn outline color='primary' size='lg'>View Cart</MDBBtn>
+</Link>
+</div>
+</div>
+);
+}
 
 export default Products;
