4efa891524f9ce476bd33ca440f758250a54e76c
balaji <balajis@gmail.com>
2023-05-11 19:19:21 +0530

    pic

--- consultancy_project/src/components/Home.js
+++ consultancy_project/src/components/Home.js
@@ -13,8 +13,11 @@ function Home()  {
     <About/>
     <Rating/>
     <br></br>
-    <br></br>
+    <br></br><div className='grid-container'>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62655.37627388419!2d76.91870783880962!3d11.041549660091412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba858fa37fede13%3A0x68395c315480220d!2sMarutham%20Bio%20AgeS%20Innovations%20(P)%20Ltd!5e0!3m2!1sen!2sin!4v1683711504837!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br></br><br></br>
+    
+    
+    </div>
     {/* <ContactForm/>
     <Admin/> */}
     {/* <ShoppingCart/> */}
