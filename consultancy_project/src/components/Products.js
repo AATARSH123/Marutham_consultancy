6be4226d253ea7a2adad9a06ec91a64b4daccf93
balaji <balajis@gmail.com>
2023-05-12 08:42:27 +0530

    Add to cart added successfully

--- consultancy_project/src/components/Products.js
+++ consultancy_project/src/components/Products.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { ImPriceTag } from 'react-icons/im';
-import { FcAbout } from 'react-icons/fc';
+import { ImPriceTag } from "react-icons/im";
+import { FcAbout } from "react-icons/fc";
 import { Link } from "react-router-dom";
-import { MDBBtn } from 'mdb-react-ui-kit';
-import CartPopup from "./Cart";
+import { MDBBtn } from "mdb-react-ui-kit";
+// import CartPopup from "./CartPopup";
 
 const Products = () => {
-
   const [product, setproduct] = useState([]);
   const [loading, setLoading] = useState(false);
   const [cartItems, setCartItems] = useState([]);
@@ -22,43 +21,30 @@ const Products = () => {
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchUserData()
-  }, [])
-
-  useEffect(() => {
-    const items = localStorage.getItem('cartItems');
-
-    if (items) {
-      
-      const cartItems = JSON.parse(items);
-      setCartItems(cartItems);
-      // continue processing cartItems data
-    } else {
-      // handle empty or null data
-    }
-    
+    fetchUserData();
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  }, [cartItems]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   function handleAddToCart(product) {
-    const existingItem = cartItems.find(item => item._id === product._id);
+    const existingItem = cartItems.find((item) => item._id === product._id);
 
     if (existingItem) {
       existingItem.quantity++;
+      setCartItems([...cartItems]);
     } else {
       setCartItems([...cartItems, { ...product, quantity: 1 }]);
     }
   }
 
   function handleRemoveFromCart(item) {
-    const updatedCartItems = cartItems.filter(cartItem => cartItem._id !== item._id);
-    setCartItems(updatedCartItems);
+    const newCartItems = cartItems.filter((cartItem) => cartItem !== item);
+    setCartItems(newCartItems);
   }
 
   function handleClearCart() {
@@ -69,44 +55,139 @@ const Products = () => {
     <div>
       <h2 className="text-3xl font-bold mb-4 text-center">Products</h2>
       {product.length > 0 && (
-        <div style={{display:"grid",gridTemplateColumns:"repeat(3, 1fr)"}}>
-          {product.map(product => (
-            <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition' key={product._id}>
-              <img className='mb-8' src={product.image} alt='' style={{width:"fit-content"}}></img>
-              <div className='mb-4 flex-gap-x-2 text-sm'>
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}>
+          {product.map((product) => (
+            <div
+              className="bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition"
+              key={product._id}
+            >
+              <img
+                className="mb-8"
+                src={product.image}
+                alt=""
+                style={{ width: "fit-content" }}
+              ></img>
+              <div className="mb-4 flex-gap-x-2 text-sm">
                 <div>{product.name}</div>
               </div>
-              <div className='flex gap-x-4 my-4'></div>
-              <div className='flex items-center gap-1'>
+              <div className="flex gap-x-4 my-4"></div>
+              <div className="flex items-center gap-1">
                 <div>
-                  <ImPriceTag/>
+                  <ImPriceTag />
                 </div>
                 <div>{product.cost}</div>
               </div>
-              <div className='flex items-center gap-1'>
+              <div className="flex items-center gap-1">
                 <div>
-                  <FcAbout/>
+                  <FcAbout />
                 </div>
                 <div>{product.shortDescription}</div>
               </div>
               <br></br>
-              <div style={{"textAlign":"center","color":"red"}}>
+              <div style={{ textAlign: "center", color: "red" }}>
                 <Link to={`/manual/${product._id}`}>Manual (Full details)</Link>
                 <div>
-                  <MDBBtn className='me-1' onClick={() => handleAddToCart(product)}>Add to cart</MDBBtn>
+                  <MDBBtn className="me-1" onClick={() => handleAddToCart(product)}>
+                    Add to cart
+                  </MDBBtn>
                 </div>
               </div>
             </div>
           ))}
         </div>
       )}
-      <div style={{ textAlign: "center" }}>
-       <Link to="/cart">
-<MDBBtn outline color='primary' size='lg'>View Cart</MDBBtn>
-</Link>
-</div>
-</div>
-);
-}
-
-export default Products;
+      {cartItems.length > 0 && (
+        <CartPopup cartItems={cartItems} onRemove={handleRemoveFromCart} onClear={handleClearCart} />
+        )}
+      </div>
+    );
+  }
+  
+  export default Products;
+  
+  function handleAddToCart(product) {
+    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const existingItem = cartItems.find((item) => item._id === product._id);
+  
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      cartItems.push({ ...product, quantity: 1 });
+    }
+  
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
+  
+  function handleRemoveFromCart(productId) {
+    let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    cartItems = cartItems.filter((item) => item._id !== productId);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }
+  
+  function handleClearCart() {
+    localStorage.removeItem("cartItems");
+  } 
+  
+  function CartPopup(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const { cartItems, onRemove, onClear } = props;
+  
+    const handleOpen = () => {
+      setIsOpen(true);
+    };
+  
+    const handleClose = () => {
+      setIsOpen(false);
+    };
+  
+    return (
+      <>
+        <div className="fixed bottom-5 right-5 bg-blue-500 p-3 rounded-full text-white cursor-pointer">
+          <span className="text-xl">{cartItems.length}</span>
+          <MDBBtn className="me-1" onClick={handleOpen}>
+            View Cart
+          </MDBBtn>
+        </div>
+        {isOpen && (
+          <div className="fixed bottom-20 right-5 p-3 bg-white border border-gray-300 shadow-lg rounded-md max-h-80 overflow-y-auto">
+            <h2 className="text-2xl font-bold mb-4">Cart Items</h2>
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item._id}>
+                  <div className="flex items-center mb-2">
+                    <img className="w-24 h-24 object-cover" src={item.image} alt={item.name} />
+                    <div className="flex flex-col ml-4">
+                      <h3 className="text-lg font-bold">{item.name}</h3>
+                      <p className="text-gray-500">{item.shortDescription}</p>
+                      <div className="flex items-center mt-2">
+                        <button className="bg-red-500 text-white px-2 py-1 rounded-full" onClick={() => onRemove(item._id)}>
+                          -
+                        </button>
+                        <span className="mx-2">{item.quantity}</span>
+                        <button className="bg-green-500 text-white px-2 py-1 rounded-full" onClick={() => handleAddToCart(item)}>
+                          +
+                        </button>
+                        <span className="mx-2">{item.cost * item.quantity}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="my-2" />
+                </li>
+              ))}
+            </ul>
+            <div className="text-right mt-4">
+              <button className="text-red-500 font-bold" onClick={onClear}>
+                Clear Cart
+              </button>
+              <button className="bg-blue-500 text-white font-bold ml-4">
+                Checkout
+              </button>
+            </div>
+            <button className="absolute top-0 right-0" onClick={handleClose}>
+              X
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
